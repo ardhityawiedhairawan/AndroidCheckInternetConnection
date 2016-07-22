4e030492edeccefd8ab3c8bb77f4e4d682637961
Ardhitya Wiedha Irawan <ardhityawiedhairawan@gmail.com>
2016-07-22 16:56:03 +0700

    Initial

---
+++ app/src/main/java/ggdc/checkinternetconection/MainActivity.java
@@ -0,0 +1,33 @@
+package ggdc.checkinternetconection;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnCheck = (Button) findViewById(R.id.btnCheck);
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConnectivityManager cm = (ConnectivityManager) getApplication().getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo netInfo = cm.getActiveNetworkInfo();
+                if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+                    Toast.makeText(getApplication(), "You are connected to "+netInfo.getTypeName()+" "+netInfo.getSubtypeName(), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplication(), "You don't have connection.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
